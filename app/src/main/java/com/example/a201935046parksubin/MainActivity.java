30353aa8132da02dbe7136c09b5b95219f43a8ff
SuBin-Park1113 <wlqkr23@naver.com>
2021-03-20 12:40:02 +0900

    subin

---
+++ app/src/main/java/com/example/a201935046parksubin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a201935046parksubin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
